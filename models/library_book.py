f61ab3f3832b9b0a34f78a90443ba16db2418644
hacaoideas <hacaoideas@gmail.com>
2020-06-14 12:37:31 +0700

    did some computed fields

--- models/library_book.py
+++ models/library_book.py
@@ -1,10 +1,12 @@
-from odoo import fields, models
+from odoo import fields, models, api
+import datetime
 
 class Book(models.Model):
     _inherit = 'library.book'
     is_available = fields.Boolean('Is Available?')
     isbn = fields.Char(help="Use valid ISBN-13 or ISBN-10")
     publisher_id = fields.Many2one(index=True)
+    review_date = fields.Date(store=True, compute='_compute_review_date', inverse='_inverse_review_date', search='_search_review_date')
 
     def _check_isbin(self):
         sef.ensure_one()
@@ -16,3 +18,16 @@ class Book(models.Model):
             return digits[-1] == check
         else:
             return super()._check_isbn()
+
+    @api.depends('date_published')
+    def _compute_review_date(self):
+        for book in self:
+            book.review_date = book.date_published + datetime.timedelta(days=15)
+
+    def _inverse_review_date(self):
+        for book in self:
+            book.date_published = book.review_date - datetime.timedelta(days=15)
+
+    def _search_review_date(self, operator, value):
+        return [('review_date',operator,value)]
+
